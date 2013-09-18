425630add4a5c53de78958cf3f4feb20b2465008
john andrey <andrey_201015@hotmail.com>
2013-09-18 11:52:44 -0500

    trabajando en la app de nodejs de prueba

--- node/cursoNode/appPrueba/app.js
+++ node/cursoNode/appPrueba/app.js
@@ -8,7 +8,11 @@ app.configure(function(){
 	app.set('view engine', 'ejs');
 	app.set('views', __dirname+'/views');
 	app.use('/static', express.static(__dirname+'/public'));
+	app.use(express.bodyParser());
 });
 
 
 app.get('/', routes.index);
+
+app.get('/agregar', routes.agregar);
+app.post('/agregar', routes.add);
--- node/cursoNode/appPrueba/routes.js
+++ node/cursoNode/appPrueba/routes.js
@@ -5,7 +5,7 @@ var db = new couchDB('agenda',{
 	passwd: ''
 });
 
-var productos = db.newDoc('productos');
+var producto = db.newDoc('producto');
 
 exports.index = function(req, res){
 	if(!req.query.q){
@@ -24,6 +24,26 @@ exports.index = function(req, res){
 		db.desing('productos', { name:'todos', params:opciones },function(resultados){
 			resultados.rows.unique();
 			res.render('index', {productos:resultados.rows});
+		});
+	}
+};
+
+exports.agregar = function(req, res){
+	res.render('agregar');
+};
+
+exports.add = function(req, res){
+	var post = req.body;
+	if(post && post.nombre && post.precio){
+		post.precio = parseInt(post.precio);
+		//post.user = req.session.user;
+		post.created = Date.parse(new Date());
+		producto.post(post, function(result){
+			if(result.ok){
+				res.redirect('/');
+			}else{
+				res.send(500, 'Algo ha salido mal');
+			}
 		});
 	}
 };
